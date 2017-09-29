f0ef3da7dc53e92c7546456ad254047b233037d8
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2017-09-29 20:23:44 -0300

    Create UsarFormulario.java

---
+++ InterfacesGraficas/UsarFormulario.java
@@ -0,0 +1,10 @@
+package usarformulario;
+
+public class UsarFormulario {
+
+    public static void main(String[] args) {
+        FormularioAluno formUm = new FormularioAluno();
+        formUm.setVisible(true);
+    }
+    
+}
