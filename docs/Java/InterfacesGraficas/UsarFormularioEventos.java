93efbdd5419820a55ccfb3b6b2d1015c3a9d9132
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2017-09-29 20:08:06 -0300

    Create UsarFormularioEventos.java

---
+++ docs/Java/InterfacesGraficas/UsarFormularioEventos.java
@@ -0,0 +1,12 @@
+
+package usarformularioeventos;
+
+public class UsarFormularioEventos {
+
+    
+    public static void main(String[] args) {
+      FormularioAlunoNovo formUmNovo = new FormularioAlunoNovo();
+      formUmNovo.setVisible(true);
+    }
+    
+}
