dae386241d63e25d89ccde785b69eddc7778aaa1
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2017-09-29 20:19:50 -0300

    Create Cadastro.java

---
+++ docs/InterfacesGraficas/InterfaceGraficaCadastrarLivro/Cadastro.java
@@ -0,0 +1,11 @@
+package cadastro;
+
+
+public class Cadastro {
+
+    public static void main(String[] args) {
+       CadastroLivro formUm = new CadastroLivro();
+       formUm.setVisible(true);
+    }
+    
+}
