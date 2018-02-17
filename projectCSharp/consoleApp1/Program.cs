05e4e8d1dd98880bf156145db0239f9b09339d06
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-02-17 12:15:21 -0200

    Add files via upload

---
+++ projectCSharp/consoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Usuario usuario1 = new Usuario();
+            Console.WriteLine(usuario1.name);
+
+            usuario1.SetName("John Smith");
+            Console.WriteLine(usuario1.name);
+
+            Usuario usuario2 = new Usuario("Sarah Jones");
+            Console.WriteLine(usuario2.name);
+
+            Console.WriteLine("Exit!");
+            Console.ReadKey();
+        }
+    }
+}
