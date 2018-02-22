9ffc0dddc1d60f312a7214338199a88bf8be71cc
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-02-21 21:02:16 -0300

    Create Aula_Select.sql

---
+++ SQL/Aula_Select.sql
@@ -0,0 +1,25 @@
+/* C1 */Select * From Filmes Order By CodFilme Asc
+
+/* C2 */select * From Filmes Order By Nome Asc
+
+/* C3 */Select * From Filmes Order By Nome Desc
+
+/* C4 */Select Nome, Categoria, Ano From Filmes Order By Ano
+
+/* C5 */Select * From Filmes Where Categoria Like 'Suspense'
+
+/* C6 */Select * From Filmes Where Categoria Like 'Suspense' Order By Nome
+
+/* C7 */Select * From Filmes Where Ano < 2000
+
+/* C8 */Select * From Filmes Where Ano < 2000 Order By Ano
+
+/* C9 */Select * From Filmes Where Categoria Like 'Comédia' And Ano > 2004 Order By Ano Asc
+
+/* C10 */Select * From Filmes Where Ano Between 2001 And 2003
+
+/* C11 */Select * From Filmes Where Ano Between 2001 And 2003 Order By Ano
+
+/* C12 */Select * From Filmes Where Nome Like '%AMOR%'
+
+/* C13 */Select * From Filmes Where Nome Like '%VINGANÇA%' And Ano = 2004
