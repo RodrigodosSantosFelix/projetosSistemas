89eea08dfc7c9e786d7c17fcf838e4e93de82f89
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-02-21 21:52:33 -0300

    Update Program.cs

--- projectCSharp/consoleApp1/Program.cs
+++ projectCSharp/consoleApp1/Program.cs
@@ -14,6 +14,19 @@ namespace ConsoleApp1
 
             Usuario usuario2 = new Usuario("Sarah Jones");
             Console.WriteLine(usuario2.name);
+            
+            //O primeiro recurso a ser adicionado é a capacidade de ler um arquivo de texto e a exibição de todo esse texto para um console.
+            static IEnumerable<string> ReadFrom(string file)
+            {
+                string line;
+                using (var reader = File.OpenText(file))
+                {
+                    while ((line = reader.Reader()) != null)
+                    {
+                        yield return line;
+                    }
+                }
+            }
 
             Console.WriteLine("Exit!");
             Console.ReadKey();
