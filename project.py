920c8b21a9a8a84a21c4cab3016f3b9d6dc2ba76
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-03-09 21:28:50 -0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,8 @@
+import webbrowser
+import time
+
+print ("Esperar para acessar a pagina")
+
+time.sleep(5)
+
+webbrowser.open("https://v.youku.com/v_show/id_XMzQ1MTU5OTc0MA==.html?spm=a2hww.20027244.m_250473.5~5!2~5~5!3~5~5~A&f=51553879")
