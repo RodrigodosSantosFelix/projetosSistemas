08fe65c444077c39ceb63030a40774091f878bcb
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-04-20 20:44:50 -0300

    Add files via upload

---
+++ projectCSharp/consoleApp1/Study1/Study1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study1
+{
+    public class CustomClass
+    {
+        public int Number { get; set; }
+
+        public int Multiply(int num)
+        {
+            return num * Number;
+        }
+
+        public CustomClass()
+        {
+            Number = 0;
+        }
+    }
+        class Program
+        {
+
+        static void Main(string[] args)
+        {
+                CustomClass custClass = new CustomClass();
+                custClass.Number = 27;
+                int result = custClass.Multiply(4);
+                Console.WriteLine($"The result is {result}.");
+        }
+    }
+}
