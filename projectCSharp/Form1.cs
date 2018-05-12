8a7d51a41b5951bc533a815a7d75f3faebf2571a
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-05-11 22:06:54 -0300

    Add files via upload

---
+++ projectCSharp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsomeCalculos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valorUm, valorDois;
+            valorUm = Convert.ToDouble(textBox1.Text);
+            valorDois = Convert.ToDouble(textBox2.Text);
+            servicoCalculos.CalcularAreasService obj = new servicoCalculos.CalcularAreasService();
+            double resultado = obj.retangulo(valorUm, valorDois);
+            label1.Text = Convert.ToString(resultado);
+            MessageBox.Show("Valor calculado " + resultado, "Uso Serviço Web", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
