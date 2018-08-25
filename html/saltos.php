c8ee170338bbca0f3206d86b7ab5e24324872e86
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2018-08-24 21:22:52 -0300

    Add files via upload

---
+++ html/saltos.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			Exibe o numero em 10 em 10
+		</title>
+	</head>
+	<body>
+		<?php
+			extract($_POST);
+			echo("valor do numero: $quantidade ");
+		?>
+	</body>
+</html>
